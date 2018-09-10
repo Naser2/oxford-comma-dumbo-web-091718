95fa7b36e74aff6c51777e2d70e94d71d6fca3cd
Nasser Sanou <nassersanou23@gmail.com>
2018-09-10 18:14:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
    array.length == 1 ? array.join()
    
- if  array.length == 2
+ : if  array.length == 2
    return array.join(" " + 'and'+ " " )
 else array.length == 3 || array.length >3
   return array[0..-2].join(', ') + array[-1].insert(0, ', and ')
